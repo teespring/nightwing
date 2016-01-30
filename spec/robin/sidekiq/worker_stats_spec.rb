37afe27b472f9f6ecc9ce2a8b63d1f232ee74b91
Chris Ledet <me@chrisledet.com>
2016-01-29 21:38:44 -0800

    specs for worker stats

---
+++ spec/robin/sidekiq/worker_stats_spec.rb
@@ -0,0 +1,31 @@
+require "rspec"
+require "robin/sidekiq/worker_stats"
+
+describe Robin::Sidekiq::WorkerStats do
+  subject { Robin::Sidekiq::WorkerStats.new }
+
+  describe "#call" do
+    context "when everything just works" do
+      it "increments process count" do
+        expect(Librato).to receive(:increment).with("robin.sidekiq.default.my_worker.processed")
+
+        subject.call("MyWorker", nil, "default", nil) do
+          # beep
+        end
+      end
+    end
+
+    context "when something fails" do
+      it "increments process and failure count" do
+        expect(Librato).to receive(:increment).with("robin.sidekiq.default.my_worker.processed")
+        expect(Librato).to receive(:increment).with("robin.sidekiq.default.my_worker.failed")
+
+        expect do
+          subject.call("MyWorker", nil, "default", nil) do
+            fail "beep"
+          end
+        end.to raise_error RuntimeError
+      end
+    end
+  end
+end
