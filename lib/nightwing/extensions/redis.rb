5c7d52dbd5bf644573138880d262e33673c4f965
Chris Ledet <me@chrisledet.com>
2016-02-13 02:15:11 -0800

    Capture redis response times

---
+++ lib/nightwing/extensions/redis.rb
@@ -0,0 +1,17 @@
+Redis::Client.class_eval do
+  def call_with_profiling(*args, &blk)
+    begin
+      start_time = Time.now
+      result = call_without_profiling(*args, &blk)
+    ensure
+      time_ellasped = (Time.now - start_time) * 1_000
+      command = args.first.is_a?(Array) ? args[0][0] : args.first
+      Nightwing.client.timing "redis.command.time", time_ellasped
+      Nightwing.client.timing "redis.command.#{command}.time", time_ellasped
+    end
+
+    result
+  end
+  alias call_without_profiling call
+  alias call call_with_profiling
+end if defined?(Redis::Client)
---
+++ spec/nightwing/extensions/redis_spec.rb
@@ -0,0 +1,42 @@
+require "spec_helper"
+
+class Redis
+  class Client
+    def call(*_)
+      @performed = true
+    end
+
+    def called?
+      @performed
+    end
+  end
+end
+
+describe "Redis extensions" do
+  subject { Redis::Client.new }
+
+  before do
+    require "nightwing/extensions/redis"
+  end
+
+  describe "#peform" do
+    it "measures time" do
+      expect(Nightwing.client).to receive(:timing) do |*args|
+        expect(args.first).to eq("redis.command.time")
+        expect(args.last > 0).to be(true)
+      end
+
+      expect(Nightwing.client).to receive(:timing) do |*args|
+        expect(args.first).to eq("redis.command.foo.time")
+        expect(args.last > 0).to be(true)
+      end
+
+      subject.call :foo
+    end
+
+    it "actually calls perform" do
+      subject.call :foo
+      expect(subject).to be_called
+    end
+  end
+end
